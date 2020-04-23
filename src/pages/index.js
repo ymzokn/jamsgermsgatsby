d1589a8f783081f707b7456a71baef8bcee42f85
Okan <ymzokn@gmail.com>
2020-04-24 00:28:37 +0300

    New Header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,18 @@
 import React from "react"
-import Page from './../layouts/page.layout';
+import Page from "./../layouts/page.layout"
 
-import "../main.scss";
+import "../main.scss"
 
-export default () => <div>
+export default () => (
+  <div>
     <Page>
-        This shows off the body font.
+      <div
+        style={{
+          height: "100rem",
+          width: "100",
+          backgroundColor: "rgb(213, 228, 227)",
+        }}
+      ></div>
     </Page>
-</div>
+  </div>
+)

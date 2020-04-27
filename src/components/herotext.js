fa252d47f89c772bd949c1930f73750ee0f2021d
Okan Yilmaz <okanyilmaz@durmazlar.com.tr>
2020-04-27 15:00:04 +0300

    Index page changes

--- src/components/herotext.js
+++ src/components/herotext.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import Button from './button';
+
 import heroStyles from "./hero.module.scss"
 
 const HeroText = () => {
     return (
         <div className={heroStyles.heroTextWrapper}>
             <h1 className={heroStyles.heroTitle}>Jam's Germs</h1>
-            <p className={heroStyles.heroDescription}>I show people the inivisible world around them. Einstein, you little devil!
-            </p>
+            <p className={heroStyles.heroDescription}>I show people the invisible world around them. Einstein, you little devil!</p>
+            <div className={heroStyles.separator}></div>
+            <Button variant="primary" text="Donate to Educate!"></Button>
         </div>
     );
 }

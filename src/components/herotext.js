7d0b806408523c91e97156a66ee02eb5149761bd
Okan Yilmaz <okanyilmaz@durmazlar.com.tr>
2020-04-28 11:17:51 +0300

    Index page changes

--- src/components/herotext.js
+++ src/components/herotext.js
@@ -9,7 +9,7 @@ const HeroText = () => {
             <h1 className={heroStyles.heroTitle}>Jam's Germs</h1>
             <p className={heroStyles.heroDescription}>I show people the invisible world around them. Einstein, you little devil!</p>
             <div className={heroStyles.separator}></div>
-            <Button variant="primary" text="Donate to Educate!"></Button>
+            <Button variant="primary_sliding" text="Donate to Educate!" ></Button>
         </div>
     );
 }

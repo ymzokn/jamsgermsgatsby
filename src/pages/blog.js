040a4c0015dc5eb41e1d608682ff543df4d10b3c
Okan Yilmaz <okanyilmaz@durmazlar.com.tr>
2020-05-05 11:18:41 +0300

    Add header image support

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,32 +1,58 @@
 import React from "react"
-import { Link, graphql, useStaticQuery } from "gatsby";
+import { Link, graphql, useStaticQuery } from "gatsby"
 import Page from "../layouts/page.layout"
-import Head from './../components/head';
+import Head from "./../components/head"
+import Img from "gatsby-image"
+import blogListStyles from "./blog.module.scss"
 
 const Blog = () => {
-
   const data = useStaticQuery(graphql`
-  query BlogPostTitles {
-    allContentfulBlogPost(sort: {fields: createdAt, order: DESC}) {
-      edges {
-        node {
-          description
-          fields {
-            slug
+    query BlogPostTitles {
+      allContentfulBlogPost(sort: { fields: createdAt, order: DESC }) {
+        edges {
+          node {
+            description
+            fields {
+              slug
+            }
+            title
+            header {
+              localFile {
+                childImageSharp {
+                  fixed(width: 200, height: 200, quality: 100) {
+                    ...GatsbyImageSharpFixed_noBase64
+                  }
+                }
+              }
+            }
           }
-          title
         }
       }
     }
-  }
   `)
 
   return (
     <Page>
       <Head title="Blog"></Head>
-      <div style={{ margin: "-5rem 25%" }}>{data.allContentfulBlogPost.edges.map(post => {
-        return <div style={{ padding: "2.5rem 1rem", marginBottom: "2.5rem", boxShadow: "0 1px 2px lightgray" }}><Link style={{ textDecoration: "none" }} to={`blog/${post.node.fields.slug}`}><h2 style={{ color: "#3a3a3a" }}>{post.node.title}</h2><p style={{ color: "darkgray" }}>{post.node.description}</p></Link></div>
-      })}</div>
+      <ul className={blogListStyles.blogList}>
+        {data.allContentfulBlogPost.edges.map(post => {
+          return (
+            <li>
+              <Link to={`blog/${post.node.fields.slug}`}>
+                <div className={blogListStyles.blogListImg}>
+                  <Img
+                    fixed={post.node.header.localFile.childImageSharp.fixed}
+                  ></Img>
+                </div>
+                <div className={blogListStyles.blogListText}>
+                  <h2>{post.node.title}</h2>
+                  <p style={{ color: "darkgray" }}>{post.node.description}</p>
+                </div>
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
     </Page>
   )
 }
--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,7 +1,8 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
 import Page from "./../layouts/page.layout"
 import Head from "./../components/head"
+import Img from "gatsby-image"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import { GoCalendar } from "react-icons/go";
 import { navigate } from "@reach/router"
@@ -11,6 +12,15 @@ export const query = graphql`
     contentfulBlogPost(fields: { slug: { eq: $slug } }) {
       title
       description
+      header {
+        localFile {
+          childImageSharp {
+            fixed(width: 1000, height: 300, quality: 100) {
+              ...GatsbyImageSharpFixed_noBase64
+            }
+          }
+        }
+      }
       body {
         json
       }
@@ -22,36 +32,39 @@ export const query = graphql`
 const Blog = props => {
   return (
     <Page>
-      <Head title={props.data.contentfulBlogPost.title}></Head>
-      <div style={{ marginTop: "-5rem", padding: "0 25%" }}>
-        <p style={{ color: "teal", textDecoration: "underline", cursor: "pointer" }} onClick={() => navigate(-1)}>Back</p>
-        {/* <Link style={{ textDecoration: "none", color: "gray" }} to="/blog">Back</Link> */}
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "baseline",
-            justifyContent: "space-between"
-          }}
-        >
-          <h1 style={{ letterSpacing: "1.5px" }}>
-            {props.data.contentfulBlogPost.title}
-          </h1>
-          <h4 style={{ margin: "1rem 0", color: "darkgray" }}>
-            {props.data.contentfulBlogPost.description}
-          </h4>
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <GoCalendar
-              style={{ width: "1.2rem", height: "1.2rem", color: "gray", marginRight: ".25rem" }}
-            />
-            <p style={{ fontStyle: "italic", color: "gray" }}>
-              {new Date(
-                props.data.contentfulBlogPost.createdAt
-              ).toLocaleDateString()}
-            </p>
+      <div style={{ marginTop: "-5rem" }}>
+        <Head title={props.data.contentfulBlogPost.title}></Head>
+        <div style={{ padding: "0 25vw" }}>
+          <p style={{ display: "inline-block", color: "teal", textDecoration: "underline", cursor: "pointer", padding: "1rem 0" }} onClick={() => navigate(-1)}>Back</p>
+          <Img fixed={props.data.contentfulBlogPost.header.localFile.childImageSharp.fixed} />
+          {/* <Link style={{ textDecoration: "none", color: "gray" }} to="/blog">Back</Link> */}
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "baseline",
+              justifyContent: "space-between"
+            }}
+          >
+            <h1 style={{ letterSpacing: "1.5px" }}>
+              {props.data.contentfulBlogPost.title}
+            </h1>
+            <h4 style={{ margin: "1rem 0", color: "darkgray" }}>
+              {props.data.contentfulBlogPost.description}
+            </h4>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <GoCalendar
+                style={{ width: "1.2rem", height: "1.2rem", color: "gray", marginRight: ".25rem" }}
+              />
+              <p style={{ fontStyle: "italic", color: "gray" }}>
+                {new Date(
+                  props.data.contentfulBlogPost.createdAt
+                ).toLocaleDateString()}
+              </p>
+            </div>
           </div>
+          <section style={{ marginTop: "2.5rem" }}>{documentToReactComponents(props.data.contentfulBlogPost.body.json)}</section>
         </div>
-        <section style={{ marginTop: "2.5rem" }}>{documentToReactComponents(props.data.contentfulBlogPost.body.json)}</section>
       </div>
     </Page>
   )

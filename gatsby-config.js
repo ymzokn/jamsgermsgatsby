dc22ba737fc7ca37edd0083af88d9d154df79357
Okan <ymzokn@gmail.com>
2020-05-31 23:03:10 +0300

    Added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ const path = require(`path`)
 module.exports = {
   siteMetadata: {
     title: "Jam's Germs",
-    author: 'Okan Yılmaz'
+    author: "Okan Yılmaz",
   },
   /* Your site config here */
   plugins: [
@@ -36,7 +36,7 @@ module.exports = {
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        downloadLocal: true
+        downloadLocal: true,
       },
     },
     {
@@ -46,7 +46,36 @@ module.exports = {
         footnotes: true,
         pedantic: true,
         gfm: true,
-        timeToRead: (wordCount, html, rawMD) => wordCount / 42
+        timeToRead: (wordCount, html, rawMD) => wordCount / 42,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-154032235-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Enables Google Optimize Experiment ID
+        // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+        // Set Variation ID. 0 for original 1,2,3....
+        // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        // Defers execution of google analytics script after page load
+        // defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "www.jamsgerms.com",
       },
     },
   ],

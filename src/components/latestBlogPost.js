8b1aad8eab2b9fed87d4f687222e6a6a4b578b01
Okan <ymzokn@gmail.com>
2020-06-04 22:35:54 +0300

    Latest wrapper knockout text

--- src/components/latestBlogPost.js
+++ src/components/latestBlogPost.js
@@ -37,16 +37,33 @@ const LatestBlogPost = () => {
   return (
     <section className={latestBlogPostStyles.latestWrapper}>
       <Link
+        className={latestBlogPostStyles.latestLink}
         to={`/blog/${data.allContentfulBlogPost.edges[0].node.fields.slug}`}
       >
-        <Img
+        <div
+          className={latestBlogPostStyles.clipText}
+          style={{
+            backgroundImage:
+              "url('http://images.ctfassets.net/4wmk22qd16cj/25y5HJoqvN7guzOyoAfKu8/d07cc361cbb307fa7113f28d6461bf18/Flamentous_Cyanobacteria_and_Algae-min.jpg')",
+          }}
+        >
+          LATEST BLOG POST
+        </div>
+        {/* <img
+          className={latestBlogPostStyles.latestImg}
+          src={
+            "http://images.ctfassets.net/4wmk22qd16cj/25y5HJoqvN7guzOyoAfKu8/d07cc361cbb307fa7113f28d6461bf18/Flamentous_Cyanobacteria_and_Algae-min.jpg"
+          }
+        ></img> */}
+
+        {/* <Img
           style={{ height: "100%" }}
           fluid={
             data.allContentfulBlogPost.edges[0].node.header.localFile
               .childImageSharp.fluid
           }
-        ></Img>
-        <div className={latestBlogPostStyles.latestTitle}></div>
+        ></Img> */}
+        {/* <div className={latestBlogPostStyles.latestTitle}></div> */}
       </Link>
     </section>
   )

e6a9df35be77932683434d0b973918a51a42b180
Okan <ymzokn@gmail.com>
2020-07-05 02:13:17 +0300

    Hero text bg photos changed

--- src/components/SubscriptionPrompt.js
+++ src/components/SubscriptionPrompt.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from "react"
-import { Context as SubscriptionContext } from "../context/subscriptionContext"
+import { Context as SubscriptionContext } from "../context/SubscriptionContext"
 import promptStyles from "../components/prompt.module.scss"
 import { IoMdClose } from "react-icons/io"
 import Button from "./button"

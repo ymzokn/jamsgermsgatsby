f12d6a6732a26fac5e73889fbd0323b8bb481e12
Okan <ymzokn@gmail.com>
2020-07-04 23:38:41 +0300

    Changed the hero text to instagram bio

--- src/layouts/page.layout.js
+++ src/layouts/page.layout.js
@@ -1,9 +1,9 @@
 import React from "react"
 import Header from "./../components/header"
 import Footer from "./../components/footer"
-import { Provider as SubscriptionProvider } from "../context/subscriptionContext"
+import { Provider as SubscriptionProvider } from "../context/SubscriptionContext"
 import layoutStyles from "../components/layout.module.scss"
-import SubscriptionPrompt from "../components/subscriptionPrompt"
+import SubscriptionPrompt from "../components/SubscriptionPrompt"
 
 const Page = props => {
   return (

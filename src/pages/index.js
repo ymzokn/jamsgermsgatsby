a5586fed31644faab4867d380be85794e1f1b777
Okan <ymzokn@gmail.com>
2020-07-11 20:33:55 +0300

    Hero styles simplified

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import Page from "./../layouts/page.layout"
 import Hero from "../components/hero"
 import Head from "../components/head"
-import ContactList from "../components/contactList"
 import "../main.scss"
 
 export default () => (
@@ -10,8 +9,6 @@ export default () => (
     <Page>
       <Head title="Home" />
       <Hero></Hero>
-      {/* <LatestBlogPosts></LatestBlogPosts> */}
-      {/* <ContactList /> */}
     </Page>
   </div>
 )

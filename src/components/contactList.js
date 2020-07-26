063b10b28d8425196f4116c6e77bd39accd2bd65
Okan <ymzokn@gmail.com>
2020-07-26 23:08:20 +0300

    Added social media accounts

--- src/components/contactList.js
+++ src/components/contactList.js
@@ -6,11 +6,17 @@ import { graphql, useStaticQuery } from "gatsby"
 
 const ContactList = () => {
   const data = useStaticQuery(graphql`
-  query InstagramLogoQuery {
+  query LogoQuery {
     instagram: file(relativePath: { eq: "instagram_logo.png" }) {
       publicURL
   },
   youtube:file(relativePath: {eq: "youtube_logo.png"}) {
+    publicURL
+  },
+  twitter:file(relativePath: {eq: "twitter_logo.png"}) {
+    publicURL
+  },
+  facebook:file(relativePath: {eq: "facebook_logo.png"}) {
     publicURL
   }
   }
@@ -26,6 +32,23 @@ const ContactList = () => {
         >
           
           <img alt="instagram-logo" src={`${data.instagram.publicURL}`}></img>
+        </a>
+        <a
+          className={contactListStyles.contact}
+          href="https://www.facebook.com/jamsgerms"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img alt="facebook-logo" src={`${data.facebook.publicURL}`}></img>
+        </a>
+        
+        <a
+          className={contactListStyles.contact}
+          href="https://twitter.com/jam_and_germs"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img alt="twitter-logo" src={`${data.twitter.publicURL}`}></img>
         </a>
         <a
           className={contactListStyles.contact}

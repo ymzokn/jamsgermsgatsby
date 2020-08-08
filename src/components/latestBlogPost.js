968cda25d948d813278136006f26be6a164e8cf4
Okan <ymzokn@gmail.com>
2020-08-08 13:10:01 +0300

    Latest blog post bg img url changed

--- src/components/latestBlogPost.js
+++ src/components/latestBlogPost.js
@@ -16,7 +16,7 @@ const LatestBlogPost = () => {
             title
             header {
               localFile {
-                url
+                publicURL
               }
             }
             fields {
@@ -37,27 +37,12 @@ const LatestBlogPost = () => {
         <div
           className={latestBlogPostStyles.clipText}
           style={{
-            backgroundImage: `url(${data.allContentfulBlogPost.edges[0].node.header.localFile.url})`,
+            backgroundImage: `url(${data.allContentfulBlogPost.edges[0].node.header.localFile.publicURL})`,
             // "url('http://images.ctfassets.net/4wmk22qd16cj/25y5HJoqvN7guzOyoAfKu8/d07cc361cbb307fa7113f28d6461bf18/Flamentous_Cyanobacteria_and_Algae-min.jpg')",
           }}
         >
           LATEST BLOG POST
         </div>
-        {/* <img
-          className={latestBlogPostStyles.latestImg}
-          src={
-            "http://images.ctfassets.net/4wmk22qd16cj/25y5HJoqvN7guzOyoAfKu8/d07cc361cbb307fa7113f28d6461bf18/Flamentous_Cyanobacteria_and_Algae-min.jpg"
-          }
-        ></img> */}
-
-        {/* <Img
-          style={{ height: "100%" }}
-          fluid={
-            data.allContentfulBlogPost.edges[0].node.header.localFile
-              .childImageSharp.fluid
-          }
-        ></Img> */}
-        {/* <div className={latestBlogPostStyles.latestTitle}></div> */}
       </Link>
     </section>
   )
